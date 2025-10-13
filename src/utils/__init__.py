5de9308d2a65b23a2240638c52816ff07eecfee5
Naterfute <naterfute@pyro.host>
2025-10-12 21:22:45 -0700

    fix: hash isn't even used yet

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,15 +1,11 @@
 from src.database.migrate import migrateDb
 from src.downloader import robyn
-from src.utils.hash import hasher
+# from src.utils.hash import hasher
 
 from robyn import Robyn
 
 
 def initapp(app: Robyn):
-    migrateDb()
-    hasher()
-    robyn(app)
-
-
-
-
+  migrateDb()
+  # hasher()
+  robyn(app)
